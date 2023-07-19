2518ed67bb3d68f4b72757310b3c934965ef19e4
kevin <onedayatatime0923@gmail.com>
2023-07-19 10:59:20 -0700

    kevin

--- average_score.py
+++ average_score.py
@@ -1,5 +1,5 @@
 import argparse
-import matplotlib
+#  import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from matplotlib.patches import Rectangle
@@ -8,27 +8,32 @@ import numpy as np
 import os
 
 
-name = ["steps = (10^3, 10^4, 10^5, 10^6, 10^7)", "architecture = ([3, 256, 5], [3, 256, 256, 5], [3, 256, 256, 256, 5], [3, 256, 256, 256, 256, 5])"]
-data = np.array([[-43.84, -293.09, -44.88, 836.91, 91.90], [-171.16, 836.91, 786.18, -270.94]])
+name = ["steps = (10^3, 10^4, 10^5, 10^6, 10^7)", "architecture = ([3, 256, 5], [3, 256, 256, 5], \n[3, 256, 256, 256, 5], [3, 256, 256, 256, 256, 5])"]
+data = [[-43.84, -293.09, -44.88, 836.91, 91.90], [-171.16, 836.91, 786.18, -270.94]]
 
 
-fig = plt.figure(figsize=(35,10))
+fig = plt.figure(figsize=(15,8))
 
-x = np.linspace(1, len(data), num=len(data))
 
-plt.bar(x, data[:, 0], width=0.15, label = "w/ branch removal", color = "crimson")
-plt.bar(x+0.2, data[:, 1], width=0.15, label = "w/o branch removal", color = "purple")
-plt.bar(x+0.4, data[:, 2], width=0.15, label = "w/o branch removal", color = "dodgerblue")
+for i, d in enumerate(data):
+    for ii, dd in enumerate(d):
+        plt.bar(i + ii * 0.15, dd, width=0.12)
+        #  x = i + np.linspace(1, len(data), num=len(data)) * 0.2
+    #  plt.bar(x, data[:, 0], width=0.15, color = "crimson")
+    #  plt.bar(x, data[:, 0], width=0.15, color = "crimson")
+    #  plt.bar(x+0.2, data[:, 1], width=0.15, color = "purple")
+    #  plt.bar(x+0.4, data[:, 2], width=0.15, color = "dodgerblue")
 
-plt.xticks(x+0.15, name, fontsize=28)
+x = np.linspace(0, len(data) - 1, num=len(data))
+plt.xticks(x+.1, name, fontsize=16)
 plt.xlabel('Parameters', fontsize=44, fontweight='bold')
 
-plt.yscale("log")
+#  plt.yscale("log")
 # plt.yticks(fontsize=28)
-plt.ylabel('Average Score over 10^2 Runs', fontsize=44, fontweight='bold')
+plt.ylabel('Average Score\nover 10^2 Runs', fontsize=44, fontweight='bold')
 #  plt.legend(loc = 'upper left', fontsize = 32)
 
-#  fig.tight_layout()
-# plt.show()
-plt.savefig("test.png", bbox_inches='tight')
+fig.tight_layout()
+#  plt.show()
+plt.savefig("average_score.png", bbox_inches='tight')
 
